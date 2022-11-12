f07575f24c7c9afe6f26a47834ce518af1d46a2b
Eric Finger <fingereric@web.de>
2022-11-12 14:29:49 +0100

    added config builder assistant

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,18 @@
 use crate::Cli;
-use confy::{get_configuration_file_path, load};
-use openssh::SessionBuilder;
+use confy::{get_configuration_file_path, load, store};
+use inquire::{
+    validator::{Validation, ValueRequiredValidator},
+    Confirm, CustomType, Editor, MultiSelect, Text,
+};
+use openssh::{Session, SessionBuilder};
 use serde::{Deserialize, Serialize};
 use tokio::runtime::Runtime;
 
-use std::path::PathBuf;
+use std::{
+    env::current_dir,
+    fmt::{Display, Formatter, Result},
+    path::PathBuf,
+};
 
 #[derive(Default, Debug, Serialize, Deserialize)]
 struct Config {
@@ -25,21 +33,45 @@ struct Config {
     remote_port: u16,
 }
 
+enum OptionalFeatures {
+    CmdBefore,
+    CmdAfter,
+    JumpHosts,
+}
+
+impl Display for OptionalFeatures {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            OptionalFeatures::CmdBefore => write!(
+                f,
+                "Run Command (locally) before establishing SSH connection"
+            ),
+            OptionalFeatures::CmdAfter => write!(
+                f,
+                "Run command (remotely) after establishing SSH connection"
+            ),
+            OptionalFeatures::JumpHosts => write!(f, "Use SSH jump-hosts"),
+        }
+    }
+}
+
 #[allow(dead_code)]
 pub struct App {
     pub cli: Cli,
     config: Config,
     directory: PathBuf,
     runtime: Runtime,
-    session: openssh::Session,
+    session: Session,
 }
 
 impl App {
     pub fn new(cli: Cli) -> Self {
-        let mut config = if get_configuration_file_path("livetunnel", "livetunnel.conf").is_err() {
+        let mut config = if cli.reconfigure
+            || get_configuration_file_path("livetunnel", "livetunnel").is_err()
+        {
             Self::build_config()
         } else {
-            load("livetunnel", "livetunnel.conf").unwrap()
+            load("livetunnel", "livetunnel").unwrap()
         };
 
         if config.host.is_empty() {
@@ -53,7 +85,7 @@ impl App {
                 panic!("Directory {:?} not found.", dir);
             }
         } else {
-            std::env::current_dir().unwrap()
+            current_dir().unwrap()
         };
 
         // TODO: Probably switch to thread pool
@@ -77,11 +109,15 @@ impl App {
             session_builder.jump_hosts(jump_hosts);
         }
 
+        // TODO: Execute Before-Commands
+
         // Connect to SSH:
         let session = match runtime.block_on(session_builder.connect(&config.host)) {
             Ok(session) => session,
             Err(error) => panic!("Couldn't establish SSH connection: {:?}", error),
         };
+
+        // TODO: Execute after commands
 
         // TODO: Webserver
 
@@ -95,8 +131,185 @@ impl App {
     }
 
     fn build_config() -> Config {
-        todo!("use inquire to get config")
+        let optional_features = vec![
+            OptionalFeatures::CmdBefore,
+            OptionalFeatures::CmdAfter,
+            OptionalFeatures::JumpHosts,
+        ];
 
-        // TODO: confy store
+        let selection = MultiSelect::new(
+            "Select which optional Features you'd like to use:",
+            optional_features,
+        )
+        .with_vim_mode(true)
+        .prompt()
+        .unwrap();
+
+        let host = Text::new("SSH Host:")
+            .with_validator(ValueRequiredValidator::default())
+            .prompt()
+            .unwrap();
+
+        let port = if Confirm::new("Set Port?")
+            .with_default(false)
+            .prompt()
+            .unwrap()
+        {
+            Some(
+                CustomType::<u16>::new("SSH Port:")
+                    .with_default(22)
+                    .with_error_message("Not a valid Port Number")
+                    .prompt()
+                    .unwrap(),
+            )
+        } else {
+            None
+        };
+
+        let username = if Confirm::new("Set Username?")
+            .with_default(false)
+            .prompt()
+            .unwrap()
+        {
+            Some(
+                Text::new("SSH user:")
+                    .with_validator(ValueRequiredValidator::default())
+                    .with_default("root")
+                    .prompt()
+                    .unwrap(),
+            )
+        } else {
+            None
+        };
+
+        let keyfile = if Confirm::new("Set Keyfile?")
+            .with_default(false)
+            .prompt()
+            .unwrap()
+        {
+            Some(
+                Text::new("SSH user:")
+                    .with_validator(|input: &str| {
+                        let path = PathBuf::from(input);
+                        if path.exists() {
+                            if path.is_file() {
+                                Ok(Validation::Valid)
+                            } else {
+                                Ok(Validation::Invalid("Not a file".into()))
+                            }
+                        } else {
+                            Ok(Validation::Invalid("The given file does not exist".into()))
+                        }
+                    })
+                    .with_placeholder("~/.ssh/id_rsa")
+                    .prompt()
+                    .unwrap()
+                    .into(),
+            )
+        } else {
+            None
+        };
+
+        let remote_port = CustomType::<u16>::new("Remote Port to forward to:")
+            .with_error_message("Not a valid Port Number")
+            .prompt()
+            .unwrap();
+
+        let local_port = CustomType::<u16>::new("Local Port to host on / forward:")
+            .with_default(3000)
+            .with_error_message("Not a valid Port Number")
+            .prompt()
+            .unwrap();
+
+        let mut before_cmd: Vec<(String, String)> = vec![];
+        let mut after_cmd: Vec<(String, String)> = vec![];
+        let mut jump_h: Vec<String> = vec![];
+
+        for entry in selection {
+            match entry {
+                OptionalFeatures::CmdBefore => {
+                    let cmd = Editor::new("Which commands should be run before making the SSH connection (One per line):")
+                        .with_validator(ValueRequiredValidator::default())
+                        .with_editor_command(std::ffi::OsStr::new("vim"))
+                        .prompt();
+
+                    if cmd.is_err() {
+                        continue;
+                    }
+
+                    for line in cmd.unwrap().lines() {
+                        let command = line.split_once(' ');
+                        match command {
+                            // (program) (Arguments)
+                            Some(x) => before_cmd.push((String::from(x.0), String::from(x.1))),
+                            None => before_cmd.push((String::from(line), String::new())),
+                        }
+                    }
+                }
+
+                OptionalFeatures::CmdAfter => {
+                    let cmd = Editor::new("Which commands should be run (remotly) after making the SSH connection (One per line):")
+                        .with_validator(ValueRequiredValidator::default())
+                        .with_editor_command(std::ffi::OsStr::new("vim"))
+                        .prompt();
+
+                    if cmd.is_err() {
+                        continue;
+                    }
+
+                    for line in cmd.unwrap().lines() {
+                        let command = line.split_once(' ');
+                        match command {
+                            // (program) (Arguments)
+                            Some(x) => after_cmd.push((String::from(x.0), String::from(x.1))),
+                            None => after_cmd.push((String::from(line), String::new())),
+                        }
+                    }
+                }
+
+                OptionalFeatures::JumpHosts => {
+                    let cmd = Editor::new("Please specify your List of Jump-Hosts (one per line):")
+                        .with_validator(ValueRequiredValidator::default())
+                        .with_editor_command(std::ffi::OsStr::new("vim"))
+                        .prompt();
+
+                    if cmd.is_err() {
+                        continue;
+                    }
+
+                    for line in cmd.unwrap().lines() {
+                        jump_h.push(String::from(line));
+                    }
+                }
+            }
+        }
+
+        let config = Config {
+            before_commands: if before_cmd.is_empty() {
+                None
+            } else {
+                Some(before_cmd)
+            },
+            after_commands: if after_cmd.is_empty() {
+                None
+            } else {
+                Some(after_cmd)
+            },
+            host,
+            port,
+            username,
+            keyfile,
+            jump_hosts: if jump_h.is_empty() {
+                None
+            } else {
+                Some(jump_h)
+            },
+            local_port,
+            remote_port,
+        };
+
+        store("livetunnel", "livetunnel", &config).unwrap();
+
+        config
     }
 }
